763359f43b0156d58ad9765a908b9b403b2f38c9
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-02-29 12:51:52 +0700

    Praktikum 2.2 Pembuatan file migrasi dengan relasi

--- database/migrations/2024_02_29_053758_create_m_kategori_tabel.php
+++ database/migrations/2024_02_29_053758_create_m_kategori_tabel.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
-            $table->string('level_kode', 10)->unique();
-            $table->string('level_nama', 100);
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10);
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
