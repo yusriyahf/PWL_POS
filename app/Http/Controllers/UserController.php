0a89871ea3436f08ae577a054b1b694344a6483f
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-03-05 19:33:07 +0700

    JS 4 | Eloquent ORM | Praktikum 2.3 - Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
     }
 }
