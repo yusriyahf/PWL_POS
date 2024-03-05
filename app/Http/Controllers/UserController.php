e40e96b26cd1ce0f58ad95a2f3bd0ff20f105600
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-03-05 17:51:42 +0700

    JS 4 | Eloquent ORM | Praktikum 2.1 - Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
