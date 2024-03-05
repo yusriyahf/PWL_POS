cfdbc8b0deb33b41319439939407f96c5c53c57d
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-03-05 19:26:23 +0700

    JS 4 | Eloquent ORM | Praktikum 2.2 - Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->first OrFail();
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
