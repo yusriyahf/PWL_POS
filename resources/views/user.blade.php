2e67dbb935079ed93d6ea573da059be4698d2a07
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-03-07 13:43:12 +0700

    JS 4 | Eloquent ORM | Praktikum 2.7 - Relationships

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,6 +15,8 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
             <th>Aksi</th>
         </tr>
         @foreach ($data as $d)
@@ -23,7 +25,9 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
-                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a><a href="/user/hapus/{{ $d->user_id }}">hapus</a></td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
+                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">hapus</a></td>
             </tr>
         @endforeach
     </table>
