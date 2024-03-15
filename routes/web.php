7588f957ae5a5e2f57d77a78113c61929295b2f8
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-03-15 21:50:27 +0700

    JS 5 | AdminLTE | Praktikum 3 - Membuat form kemudian menyimpan data dalam database

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,6 @@
     Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
     Route::get('/kategori', [KategoriController::class, 'index']);
+
+    Route::get('/kategori/create', [KategoriController::class, 'create']);
+    Route::post('/kategori', [KategoriController::class, 'store']);
