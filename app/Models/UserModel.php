4b5a81b76fdad2a7eaf87336929694158ea8568c
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-04-24 23:53:29 +0700

    JS 10 | Api Bagian 1 | Praktikum 1 – Membuat RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,16 +4,26 @@ namespace App\Models;
 
 use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as UserAuthenticate;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
  * @mixin IdeHelperUserModel
  */
-class UserModel extends UserAuthenticate
+class UserModel extends UserAuthenticate implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
