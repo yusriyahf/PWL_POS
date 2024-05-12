84f84a1813c60762068e86ce637c4674a84707ce
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-05-12 23:12:17 +0700

    JS 12 | FileUpload | F - METHOD MOVE

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -19,9 +19,12 @@ class FileUploadController extends Controller
         ]);
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-' . time() . "." . $extfile;
-        $path = $request->berkas->storeAs('public', $namaFile);
 
-        $pathBaru = asset('storage/' . $namaFile);
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path = str_replace("\\", "//", $path);
+        echo "Variabel path berisi:$path <br>";
+
+        $pathBaru = asset('gambar/' . $namaFile);
         echo "proses upload berhasil, data disimpan pada: $path";
         echo "<br>";
         echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
