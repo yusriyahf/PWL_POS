a3feb0284092a72db0bb0e0260ed26aaf00a7d19
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-05-12 21:14:12 +0700

    JS 12 | FileUpload | C. VALIDASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -16,20 +16,9 @@ class FileUploadController extends Controller
         // dump($request->berkas);
         // dump($request->file('file));
         // return "Pemrosesan file upload di sini;
-        if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        $request->validate([
+            'berkas' => 'required|file|image|max:500',
+        ]);
+        echo $request->berkas->getClientOriginalName() . "lolos validasi";
     }
 }
